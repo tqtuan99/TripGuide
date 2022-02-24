ee079ead78c3b61f08034bda420b9ebc32d24a9c
Trần Quốc Tuấn <quoctuancr1010@gmail.com>
2022-02-24 18:11:46 +0700

    js

--- js/app.js
+++ js/app.js
@@ -5,6 +5,21 @@ $(".tour-list").slick({
    prevArrow: "<button type='button' class='slick-prev pull-left'><i class='fas fa-angle-left'></i></button>",
    nextArrow: "<button type='button' class='slick-next pull-right'><i class='fas fa-angle-right'></i></button>",
    autoplay: true,
+   responsive: [{
+         breakpoint: 1025,
+         settings: {
+            slidesToShow: 2,
+         },
+      },
+      {
+         breakpoint: 480,
+         settings: {
+            slidesToShow: 1,
+            arrows: false,
+            infinite: false,
+         },
+      },
+   ],
 });
 $(".explore-list").slick({
    infinite: true,
@@ -13,6 +28,21 @@ $(".explore-list").slick({
    prevArrow: "<button type='button' class='slick-prev pull-left'><i class='fas fa-angle-left'></i></button>",
    nextArrow: "<button type='button' class='slick-next pull-right'><i class='fas fa-angle-right'></i></button>",
    // autoplay: true,
+   responsive: [{
+         breakpoint: 1025,
+         settings: {
+            slidesToShow: 2,
+         },
+      },
+      {
+         breakpoint: 480,
+         settings: {
+            slidesToShow: 1,
+            arrows: false,
+            infinite: false,
+         },
+      },
+   ],
 });
 
 //Custom Button
@@ -31,26 +61,49 @@ function dec(element) {
 var model = document.querySelector('.location-model')
 var searchLocation = document.querySelector('.search-location')
 
-searchLocation.onclick = function () {
+searchLocation.onclick = function (event) {
    model.style.display = 'flex';
+   event.stopPropagation();
 }
+
 window.onclick = function (event) {
-   if (event.target == model) {
-      model.style.display = "none";
+   if (event.target.element == model || event.target.element == showPassengerModel) {
+      // if(model.style.display == 'flex')
+         model.style.display = 'none';
+      // if(showPassengerModel.style.display== 'flex')
+         showPassengerModel.style.display= 'none'
    }
 }
 
 let fillDatas = document.querySelectorAll('.item-detail')
-for(const fillData of fillDatas){
-   fillData.onclick = function(){
-      fillsquare()
+for (const fillData of fillDatas) {
+   fillData.onclick = function () {
+      fillsquare(fillData);
+      model.style.display = 'none';
    }
 }
 
-function fillsquare(input, output) {
-   var area = area;
-   var square = square;
+var inputData = document.getElementById('local')
 
-   document.getElementById('local').value = input;
-   document.getElementsByClassName('item-detail-name').value = output;
+function fillsquare(element) {
+   let input = element.textContent;
+   input = input.replace(/^\s+|\s+$/gm, ' ');
+   input = input.trim();
+   input = input.replace(/^\s+|\s+$/gm, ', ');
+   inputData.value = input;
+}
+
+let showPassenger = document.querySelector('.search-pass')
+let showPassengerModel = document.querySelector('.passenger-model')
+let passengerIcon = document.querySelector('.passengerIcon')
+showPassenger.onclick = function () {
+   if (showPassengerModel.style.display == 'flex') {
+      showPassengerModel.style.display = 'none';
+      passengerIcon.classList.remove('fa-chevron-up');
+      passengerIcon.classList.add('fa-chevron-down');
+   } else {
+      showPassengerModel.style.display = 'flex';
+      passengerIcon.classList.remove('fa-chevron-down');
+      passengerIcon.classList.add('fa-chevron-up');
+   }
 }
